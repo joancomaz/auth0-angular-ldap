025cec6490b699361e2176414b1de339293c99fd
joancomaz <joancordoba@gmail.com>
2020-04-19 23:31:54 +0200

    Agregamos las rutas de nuestro proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { ProtectedComponent } from './pages/protected/protected.component';
+
+const ROUTES: Routes = [
+  { path: 'public-page-1', component: Page1Component },
+  { path: 'public-page-2', component: Page2Component },
+  { path: 'protected-page', component: ProtectedComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'public-page-1' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(ROUTES)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,4 +13,8 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  login() {
+    console.log('Botón login pulsado');
+  }
+
 }
